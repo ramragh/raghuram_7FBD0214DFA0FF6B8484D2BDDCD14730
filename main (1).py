d9d9fa15b0182de711292a0024a4ef8fff039cf4
ramragh <144221106+ramragh@users.noreply.github.com>
2023-09-17 10:20:00 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,12 +1,29 @@
-#program to check a leap yar using elif
+'''Implement a class called Player that represents a cricket player. The Player class should have a
+method called play() which prints "The player is playing cricket. Derive two classes, Batsman and
+Bowler, from the Player class. Override the play() method in each derived class to print "The batsman
+is batting" and "The bowler is bowling", respectively. Write a program to create objects of both the
+Batsman and Bowler classes and call the play() method for each object.'''
 
-year=int(input("please enter the year number (eg:2019):"))
 
-if(year%400==0):
-      print("%d is a leap year"%year)
-elif(year%100==0):
-      print("%d is a leap year"%year)
-elif(year%4==0):
-      print("%d is Leap Year"%year)
-else:
-      print("%d is Not the Leap Year"%year)
+# Define the base class Player
+class Player:
+    def play(self):
+        print("The player is playing cricket.")
+
+# Define the derived class Batsman
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting.")
+
+# Define the derived class Bowler
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling.")
+
+# Create objects of Batsman and Bowler classes
+batsman = Batsman()
+bowler = Bowler()
+
+# Call the play() method for each object
+batsman.play()
+bowler.play()
